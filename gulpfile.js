3e65a9e52856321679259517b19e0e2bcdf5f130
Blaine Bublitz <blaine.bublitz@gmail.com>
2019-02-24 21:20:53 -0700

    use dest as a passthrough

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,18 @@
 const gulp = require('gulp');
 const gulpBabel = require('gulp-babel');
-const gulpClone = require('gulp-clone');
 const gulpGzip = require('gulp-gzip');
 const pump = require('pump');
-const merge2 = require('merge2');
-
-const sourcemapWrite = require('vinyl-fs/lib/dest/sourcemap');
 
 gulp.task('default', () => {
-	const stream = pump(
+	// I'd highly recommend using `require('stream').pipeline`
+	// because pump doesn't error correctly on the returned stream
+	return pump(
 		gulp.src('src/**', {sourcemaps: true}),
 		gulpBabel({
 			presets: ['minify']
 		}),
-		sourcemapWrite({
-			resolve: () => '.'
-		})
-	);
-
-	const merged = merge2(
-		stream,
-		pump(
-			stream,
-			gulpClone(),
-			gulpGzip()
-		)
-	);
-
-	return pump(
-		merged,
-		gulp.dest('wwwroot')
+		gulp.dest('wwwroot', { sourcemaps: '.' }),
+		gulpGzip(),
+		gulp.dest('wwwroot'),
 	);
 });
